5013b893523afe2c59142fb6c2e61cd0fb320ab0
EricDMW <wdong025@ucr.edu>
2024-12-12 20:02:52 -0800

    update env and algorithm

---
+++ algorithm/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@File   : __init__.py
+@Author : Dongming Wang
+@Email  : dongming.wang@email.ucr.edu
+@Project: Coupled RL
+@Date   : 12/12/2024
+@Time   : 20:02:08
+@Info   : Portal of algorithm
+"""
+
+
---
+++ env/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@File   : __init__.py
+@Author : Dongming Wang
+@Email  : dongming.wang@email.ucr.edu
+@Project: coupled RL
+@Date   : 12/12/2024
+@Time   : 20:00:33
+@Info   : portal of env
+"""
+
+
